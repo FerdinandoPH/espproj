edbeccf59f9c22bc8388e9f525576f55892872ea
FerdinandoPH <perez.holguin@gmail.com>
2022-11-12 14:03:43 +0100

    muchos retoques

--- weatherchanger.py
+++ weatherchanger.py
@@ -1,30 +1,40 @@
 import requests,json,time
-# Read from GET https://api.openweathermap.org/data/2.5/weather?lat=40.488097&lon=-3.343781&appid=4a9f0193f2c492fb37368cbf04b93485, take the values of wather/main and main/temp and print them
+# Read from GET https://api.openweathermap.org/data/2.5/forecast?lat=40.488097&lon=-3.343781&appid=4a9f0193f2c492fb37368cbf04b93485, take the value pop of the next day at 12:00 and return it
 
-def get_weather():
-    url = "https://api.openweathermap.org/data/2.5/weather?lat=40.488097&lon=-3.343781&appid=4a9f0193f2c492fb37368cbf04b93485"
-    response = requests.get(url)
-    data = json.loads(response.text)
-    status=response.status_code
-    if status == 200:
-        weather = data['weather'][0]['main']
-        temp = round(data['main']['temp']-273,2)
+def get_weather(datos=False):
+    url="https://api.openweathermap.org/data/2.5/forecast?lat=40.488097&lon=-3.343781&appid=4a9f0193f2c492fb37368cbf04b93485"
+    response=requests.get(url)
+    data=response.json()
+    hourIndex=0
+    for i in range(0,len(data["list"])):
+        if data["list"][i]["dt_txt"][11:13]=="12" and data["list"][i]["dt_txt"][8:10]==str(int(time.strftime("%d"))+1):
+            hourIndex=i
+            break
+    if datos:
+        print(data)
+        print("\n\n")
+        print(data["list"][hourIndex])
+    #Get the probability of rain for the next day at 12:00. The data is in 3 hour intervals, so determine the index of the next day at 12:00
+    #The data is in 3 hour intervals, so determine the index of the next day at 12:00
 
-        print(weather)
-        print(temp)
-    mainweather="https://api.thingspeak.com/update?api_key=E883ZKIT1R3NIRMV&field1="+str(weather)
-    response2=requests.get(mainweather)
-    while response2.text==0:
-        print("retrying")
-        response2=requests.get(mainweather)
-        time.sleep(1)
-    time.sleep(20)
-    temp="https://api.thingspeak.com/update?api_key=E883ZKIT1R3NIRMV&field2="+str(temp)
-    response3=requests.get(temp)
-    while response3.text==0:
-        print("retrying")
-        response3=requests.get(temp)
-        time.sleep(1)
-    time.sleep(20)
-while True:
-    get_weather()
+
+    pop=data["list"][hourIndex]["pop"]
+    
+    print(pop)
+    #Now send pop to GET https://api.thingspeak.com/update?api_key=E883ZKIT1R3NIRMV&field1=0
+    url="https://api.thingspeak.com/update?api_key=E883ZKIT1R3NIRMV&field1="+str(pop)
+    response=requests.get(url)
+    if response.status_code==200:
+        print("OK")
+    else:
+        while response.status_code!=200:
+            print("Reintentando")
+            response=requests.get(url)
+            print(response.status_code)
+            time.sleep(1)
+            
+    time.sleep(86300)
+if __name__=="__main__":
+    while True:
+        get_weather()
+
--- web/script.js
+++ web/script.js
@@ -1,23 +1,33 @@
 function ActualizaCampo(campo){
-    //Display the data recevied from GEThttps://api.thingspeak.com/channels/1896618/fields/5/last.txt at the paragraph with id "humedad_suelo" 
-    correspondencia=["luz","temperatura","humedad","pulsador","humedad_suelo"];
-    textoalprincipio=["Luz: ","Temperatura: ","Humedad: ","Pulsador: ","Humedad del suelo: "];
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+    //Actualiza el campo indicado desde thingspeak
+    correspondencia=["luz","temperatura","humedad","pulsador","humedad_suelo","regando"];
+    textoalprincipio=["Luz: ","Temperatura: ","Humedad: ","Pulsador: ","Humedad del suelo: ","Regando: "];
+    var recibeCampos = new XMLHttpRequest();
+    recibeCampos.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            if (campo==6){
+                if (this.responseText=="0"){
+                    document.getElementById(correspondencia[campo-1]).innerHTML = textoalprincipio[campo-1]+"No";
+                }else{
+                    document.getElementById(correspondencia[campo-1]).innerHTML = textoalprincipio[campo-1]+"Si";
+                }
+            }
+            else{
             document.getElementById(correspondencia[campo-1]).innerHTML = textoalprincipio[campo-1]+this.responseText;
+            }
         }
         };
-    xhttp.open("GET", "https://api.thingspeak.com/channels/1896618/fields/"+campo+"/last.txt", true);
-    xhttp.send();
+    recibeCampos.open("GET", "https://api.thingspeak.com/channels/1896618/fields/"+campo+"/last.txt", true);
+    recibeCampos.send();
 
     
     
 }
 function ActualizaTiempo(){
-    // Display the data received from GET https://api.openweathermap.org/data/2.5/weather?lat=40.48&lon=-3.34}&appid=4a9f0193f2c492fb37368cbf04b93485 at the paragraph with id "tiempo"
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+    //Recibe el tiempo desde openweather
+    var recibeTiempo = new XMLHttpRequest();
+    recibeTiempo.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var tiempo = JSON.parse(this.responseText);
             console.log(tiempo);
@@ -25,18 +35,21 @@ function ActualizaTiempo(){
             document.getElementById("tiempo").innerHTML = mensajeTiempo;
         }
         };
-    xhttp.open("GET", "https://api.openweathermap.org/data/2.5/weather?lat=40.488097&lon=-3.343781&appid=4a9f0193f2c492fb37368cbf04b93485", true);
-    xhttp.send();
+    recibeTiempo.open("GET", "https://api.openweathermap.org/data/2.5/weather?lat=40.488097&lon=-3.343781&appid=4a9f0193f2c492fb37368cbf04b93485", true);
+    recibeTiempo.send();
 }
 function ActualizaDatos(){
     console.log("Actualizando datos a las "+new Date());
-    for (i=1;i<6;i++){
+    for (i=2;i<7;i++){
         ActualizaCampo(i);
     }
     
 }
 
 window.onload=function(){
+    //Esconde el párrafo de pulsador (era sólo de prueba)
+    document.getElementById("pulsador").style.display="none";
+    //Actualiza los datos cada 80 segundos
     ActualizaDatos();
     ActualizaTiempo();
     setInterval(ActualizaDatos, 80000);
